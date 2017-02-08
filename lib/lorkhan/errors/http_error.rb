99e579b2c3ab132a723c1db2bd9423ee1d972457
Skylar Schipper <me@skylarsch.com>
2017-02-08 11:33:59 -0800

    Update http_error.rb

--- lib/lorkhan/errors/http_error.rb
+++ lib/lorkhan/errors/http_error.rb
@@ -1,10 +1,11 @@
 module Lorkhan
   module Errors
     class HTTPError < Base
-      attr_reader :status
+      attr_reader :status, :response
 
       def initialize(response)
         super("http_error/#{response.status}")
+        @response = response
         @status = response.status
       end
     end
